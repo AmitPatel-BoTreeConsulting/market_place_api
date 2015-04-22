4e88c171d9fe8a6f9c5ef1353b8549b003d219af
Amit Patel <amit.patel@botreeconsulting.com>
2015-04-22 16:32:17 +0530

    Adds the user create endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Api::V1::UsersController, type: :controller do
   before(:each) { request.headers['Accept'] = 'application/vnd.marketplace.v1' }
 
-  describe 'Get #show' do
+  describe 'GET #show' do
     before(:each) do
       @user = FactoryGirl.create :user
       get :show, id: @user.id, format: :json
@@ -15,5 +15,45 @@ describe Api::V1::UsersController, type: :controller do
     end
 
     it { should respond_with 200}
+  end
+
+  describe 'POST #create' do
+    context 'when is successfully created' do
+      it 'renders the json representation for the user record just created' do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        
+        expect(user_response[:email]).to eql @user_attributes[:email]
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'when is not created' do
+      before(:each) do
+        #notice I'm not including the email
+        @invalid_user_attributes = { password: "12345678",
+                                     password_confirmation: "12345678" }                                    
+      end
+
+      it 'renders an errors json' do
+        post :create, { user: @invalid_user_attributes }, format: :json 
+
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        
+        expect(user_response).to have_key(:errors)
+        expect(response).to have_http_status(422)
+      end
+
+      it 'renders the json errors on why the user could not be created' do
+        post :create, { user: @invalid_user_attributes }, format: :json 
+
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        
+        expect(user_response[:errors][:email]).to include "can't be blank"
+        expect(response).to have_http_status(422)
+      end
+    end
   end
 end
